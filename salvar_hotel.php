b0d87882a1e8fdc2620029ff98240259140cb144
carlao001 <0001116824@senaimgaluno.com.br>
2026-09-15 21:25:32 -0300

    Modificação dos códigos de Hotel

--- salvar_hotel.php
+++ salvar_hotel.php
@@ -3,9 +3,11 @@ require_once 'conexao.php';
 $nome = $_POST['nome_hotel'];
 $cidade = $_POST['cidade'];
 $estrelas = $_POST['estrelas'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
-$sql = "INSERT INTO hoteis (nome, cidade, estrelas)
-VALUES ('$nome', '$cidade', '$estrelas')";
+$sql = "INSERT INTO hoteis (nome, cidade, estrelas, email, senha)
+VALUES ('$nome', '$cidade', '$estrelas', '$email', '$senha')";
 
 if(mysqli_query($conexao, $sql)){
 
